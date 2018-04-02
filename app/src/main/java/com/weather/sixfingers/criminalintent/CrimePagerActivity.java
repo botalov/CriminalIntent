76587cb478673d60c6798c4e31bee731251efb07
botalov <botalovnikita@gmail.com>
2018-04-02 22:04:15 +0500

    ViewPager (go to first and last crimes)

--- app/src/main/java/com/weather/sixfingers/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/weather/sixfingers/criminalintent/CrimePagerActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +23,9 @@ public class CrimePagerActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
 
+    private Button mFirstButton;
+    private Button mLastButton;
+
     public static Intent newIntent(Context packageContext, UUID crimeId){
         Intent intent = new Intent(packageContext, CrimePagerActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
@@ -32,6 +37,9 @@ public class CrimePagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
 
+        mFirstButton = findViewById(R.id.start_crime_button);
+        mLastButton = findViewById(R.id.last_crime_button);
+
         UUID crimeId = (UUID)getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = findViewById(R.id.crime_view_pager);
@@ -41,6 +49,7 @@ public class CrimePagerActivity extends AppCompatActivity {
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
+
                 Crime crime = mCrimes.get(position);
                 return CrimeFragment.newInstance(crime.getId());
             }
@@ -48,6 +57,37 @@ public class CrimePagerActivity extends AppCompatActivity {
             @Override
             public int getCount() {
                 return mCrimes.size();
+            }
+
+
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 0){
+                    mFirstButton.setEnabled(false);
+                }
+                else{
+                    mFirstButton.setEnabled(true);
+                }
+
+                if(position == mCrimes.size() - 1){
+                    mLastButton.setEnabled(false);
+                }
+                else{
+                    mLastButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
 
@@ -57,5 +97,13 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
+    }
+
+    public void goToFirstCrime(View view){
+        mViewPager.setCurrentItem(0);
+    }
+
+    public void goToLastCrime(View view){
+        mViewPager.setCurrentItem(mCrimes.size() - 1);
     }
 }
